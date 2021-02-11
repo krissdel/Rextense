74df3c25f39f02d965d9f26838447f3c77cab7c1
christian <delormechrist@gmail.com>
2021-02-11 23:00:20 +0100

    lien calender

--- js/index.js
+++ js/index.js
@@ -1,6 +1,41 @@
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+// var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
+
+
+function rextenseContact() {
+
+console.log("hgjfkdls");
+   
+document.rextenseContact.action='mailto:delormechrist@gmail.com?subject="rextense"';
+console.log(document.rextenseContact.action);
+
+
+
+    // // par défaut, pas d'envoi
+    // document.formu.action="";
+    // // recherche de @ dans l'adresse
+    // var address=window.document.formu.adr.value;
+    // var pos=address.indexOf("@");
+    // if (address=="") {
+    //  alert("Vous devez donner une adresse e-mail.");
+    // }
+    // else {
+    //  if (pos==-1 || pos==0 || pos==address.length-1) {
+    //   alert("Une adresse e-mail a le format nom@domaine");
+    //  }
+    //  else // tout va bien, on envoie le formulaire
+    //   document.formu.action='mailto:moi@ici.fr?subject="Cours JavaScript"';
+    // }
+   
+   
+
+
+
+
+}                                 
+   
+ 
